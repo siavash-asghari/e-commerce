71391dd8bd616b228ff5a5a11929b981433e52b5
siavash asghari <siavashas@gmail.com>
2022-01-25 18:11:21 +0330

    update contact componenet

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import support from '../assets/image/support.png';
@@ -9,38 +9,52 @@ import '../assets/css/contact.css'
 
 
 const Contact = () => {
+
+const [validated, setValidated] = useState(false);
+
+const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    
+    setValidated(true);
+  };
+
+
     return (
         <div className='container my-5 p-2 border border-2 bgContact'>
             <div className='row'>
-                <div className='d-flex flex-column justify-content-center align-items-center p-3 col-xl-12 col-lg-12 col-md-12 col-sm-12'>
+                <div className=' p-3 col-xl-12 col-lg-12 col-md-12 col-sm-12 headerContact'>
                     <h2>تماس با ما</h2>
-                    <p>لطفاً پیش از ارسال ایمیل یا تماس تلفنی، ابتدا پرسش‌های متداول را مشاهده کنید.</p>
                 </div>
             </div>
-                <hr />
-            <div className='row'>
-                    <div className='col-xl-5 col-lg-6 col-md-6 col-sm-12'>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>نام و نام خانوادگی</Form.Label>
-                                <Form.Control type="email" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>ایمیل</Form.Label>
-                                <Form.Control type="email" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>پیام</Form.Label>
-                                <Form.Control as="textarea" rows={3} />
-                            </Form.Group>
-                            <Button variant="primary" type="submit">
-                                ارسال
-                            </Button>
-                        </Form>
-                    </div>
-                    <div className='d-flex justify-content-center align-items-center col-xl-7 col-lg-6 col-md-6 col-sm-12 imgContact'>
-                        <img src={support} alt='contact' height="265px" />
-                    </div>
+            <hr />
+            <div className='row p-3'>
+                <div className='col-xl-5 col-lg-6 col-md-6 col-sm-12 imgContact'>
+                    <img src={support} alt='contact' height="265px" />
+                </div>
+                <div className='col-xl-5 col-lg-6 col-md-6 col-sm-12'>
+                    <Form noValidate validated={validated} onSubmit={handleSubmit} >
+                        <Form.Group className="mb-3" controlId="formName">
+                            <Form.Label>نام و نام خانوادگی</Form.Label>
+                            <Form.Control type="text" required />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formEmail">
+                            <Form.Label>ایمیل</Form.Label>
+                            <Form.Control type="email" required />
+                            <Form.Control.Feedback type='invalid'>لطفا ایمیل را درست وارد کنید</Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formPassword">
+                            <Form.Label>پیام</Form.Label>
+                            <Form.Control as="textarea" rows={4} required minLength="10" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            ارسال
+                        </Button>
+                    </Form>
+                </div>
             </div>
         </div>
     )
