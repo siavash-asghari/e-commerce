e9fcc02aff7f75dd9039ec4e25454440dd7893c6
siavash asghari <siavashas@gmail.com>
2022-02-03 23:03:16 +0330

    change banner

--- src/components/Banners.jsx
+++ src/components/Banners.jsx
@@ -1,44 +1,84 @@
-import React from 'react';
+import React from 'react'
 import image01 from '../assets/image/category-1.jpg'
 import image02 from '../assets/image/category-2.jpg'
 import image03 from '../assets/image/category-3.jpg'
 import image04 from '../assets/image/category-4.jpg'
 import image05 from '../assets/image/category-5.jpg'
-import '.././assets/css/banners.css';
+import '.././assets/css/banners.css'
+import Carousel from 'react-bootstrap/Carousel'
+import slider01 from '../assets/image/slider01.jpg'
+import slider02 from '../assets/image/slider02.jpg'
+import slider03 from '../assets/image/slider03.jpg'
 
 const Banners = () => {
-    return (
-        <section>
-            <div className='cont'>
-               <div className='row mask'>
-                <div className='col-lg-6 col-md-6 col-sm-12 image1'>
-                    <img className='img1' alt='image01' src={image01} />
-                </div>
+  return (
+    <section>
+      <div className="cont">
+        <div className="row mask">
+          <div className="col-lg-6 col-md-12 col-sm-12 image1">
+            {/* <img className='img1' alt='image01' src={image01} /> */}
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={slider01}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>
+                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={slider02}
+                  alt="Second slide"
+                />
 
-                <div className='row col-lg-6 col-md-6 col-sm-12 image2 images'>
-                    <div className='col-lg-6 col-md-6 col-sm-6 img2'>
-                        <img alt='image01' src={image02} />
-                    </div>
-                    <div className='col-lg-6 col-md-6 col-sm-6 img2'>
-                        <img alt='image01' src={image03} />
-                    </div>
-                    <div className='col-lg-6 col-md-6 col-sm-6 img2'>
-                        <img alt='image01' src={image04} />
-                    </div>
-                    <div className='col-lg-6 col-md-6 col-sm-6 img2'>
-                        <img alt='image01' src={image05} />
-                    </div>
-                </div>
-                    </div>
+                
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={slider03}
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>
+                    Praesent commodo cursus magna, vel scelerisque nisl
+                    consectetur.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+
+          <div className="row col-lg-6 col-md-12 col-sm-12 image2 images">
+            <div className="col-lg-6 col-md-6 col-sm-6 img2">
+              <img alt="image01" src={image02} />
             </div>
-        </section>
-        
-    )
+            <div className="col-lg-6 col-md-6 col-sm-6 img2">
+              <img alt="image01" src={image03} />
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-6 img2">
+              <img alt="image01" src={image04} />
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-6 img2">
+              <img alt="image01" src={image05} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default Banners;
-
-
+export default Banners
 
 // import React from 'react';
 // import image01 from '../assets/image/category-1.jpg'
@@ -74,7 +114,7 @@ export default Banners;
 
 //             </div>
 //         </section>
-        
+
 //     )
 // }
 
